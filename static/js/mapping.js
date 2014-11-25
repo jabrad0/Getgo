66c5121959bf335b996d162cb3308c4a6d1c6974
jabrad0 <julibrady@gmail.com>
2014-11-25 12:26:59 -0800

    Direction buttons are working for both yellow and green/purple pins, still want to add button to end stoll nad present user with log of locations and milage along with other bells and whistles

--- static/js/mapping.js
+++ static/js/mapping.js
@@ -21,17 +21,9 @@ var infowindows = {};
   var infowindow = new google.maps.InfoWindow();
   var markers = [];
 
-  var handleBusinessResults = function(data) {
-          //above invokes ajax call?
-           //console.logs a json object
-
-          //iterate over data which = webapp.py return value yelp_api_call_json" 
-          //pull out lat and long and turn into pins on map '''
-
-          //console.log("here I am!", Object.keys(data))  // == dict.keys() in python but JS is function and must enter name of array here it is 'data'
-          //convert object to array and forEach itterates over, basically  
-    var i = 0;
-    Object.keys(data).forEach(function(title) {
+  var handleBusinessResults = function(data, lat_starting, lng_starting){
+    Object.keys(data)
+    .forEach(function(title) {
       var attributes = data[title];
       if (attributes.categories[0][0] === "Art Galleries") {
         var icon = 'http://maps.google.com/mapfiles/ms/icons/green-dot.png';
@@ -46,23 +38,76 @@ var infowindows = {};
         title: title,
         icon: icon
       });
-      infowindows[title] = infowindow;
+   
       google.maps.event.addListener(marker_businesses, 'click', function() {
-        var infowindow_content = '<h3>' + title + '</h3><div class="place">'+ attributes.address[0] + '<br />' + attributes.categories[0][0] + '<br />' + '<a href="' + attributes.url + '">Yelp Link</a>' + '<button id = "' + i + '" class="get-directions-button" data-title="'+title +'" data-lat1="'+lat_starting +'" data-long1="' +lat_starting +'" data-lat2="' +attributes.latitude+'" data-long2="' +attributes.longitude+'">Directions</button></div>';
-
-        infowindow.setContent(infowindow_content);
-
-         // $("body").append($("<h3>").attr("id", "title").val(title));
-        // $("body").append($("<button>").click(function(){alert("heya!");}));
-          // $("button")
-
-
-        //alert("clicked " + attributes.latitude);
-        //infowindow.setContent(this.info);  could also do this
+        var header = $('<h3></h3>');
+        header.text(title);
+        var content = $('<div></div>');
+        var address = $('<div></div>');
+        address.text(attributes.address[0]);
+        var categories = $('<div></div>');
+        categories.text(attributes.categories[0][0]);
+        var url = $('<div><a href="' + attributes.url + '">Yelp Link</a></div>');
+        var directions = $('<button class="info-window-direction-button" />');
+        directions.text('Directions');
+        content.append(header);
+        content.append(address);
+        content.append(categories);
+        content.append(url);
+        content.append(directions);
+        
+        infowindow.setContent(content.html()); 
         infowindow.open(map, marker_businesses);
-      }); //end of line 118
+        $('.info-window-direction-button').click(function() {
+          renderDirections([lat_starting, lng_starting], [attributes.latitude, attributes.longitude])
+        });
+      }); 
       markers.push(marker_businesses);
-    }); //end of the for loop 
+    }); 
+  };
+
+  var handleArtResults = function(data, lat_starting, lng_starting){
+    var bounds = new google.maps.LatLngBounds(new google.maps.LatLng(lat_starting - 0.01, lng_starting - 0.01), new google.maps.LatLng(lat_starting + 0.01, lng_starting + 0.01)); 
+    Object.keys(data)
+    .filter(function(title) {
+      return bounds.contains(new google.maps.LatLng(data[title].latitude, data[title].longitude));
+    })
+    .forEach(function(title){
+      var attributes = data[title];
+      //console.log(attributes.url);
+      var marker_public_art = new google.maps.Marker({
+        map: map,
+        animation: google.maps.Animation.DROP,
+        position: new google.maps.LatLng(attributes.latitude, attributes.longitude),
+        icon: 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png',
+      });
+      
+      google.maps.event.addListener(marker_public_art, 'click', function() {
+        var header = $('<h3></h3>');
+        header.text(title);
+        var content = $('<div></div>');
+        var address = $('<div></div>');
+        address.text(attributes.address);
+        var media_detail = $('<div></div>');
+        media_detail.text(attributes.media_detail);
+        var exterior = $('<div></div>');
+        exterior.text(attributes.exterior);
+        var directions = $('<button class="info-window-direction-button" />');
+        directions.text('Directions');
+        content.append(header);
+        content.append(address);
+        content.append(media_detail);
+        content.append(exterior);
+        content.append(directions);
+        infowindow.setContent(content.html()); 
+        infowindow.open(map, marker_public_art);
+        console.log(lat_starting, lng_starting);
+        $('.info-window-direction-button').click(function() {
+          renderDirections([lat_starting, lng_starting], [attributes.latitude, attributes.longitude])
+        });
+      });
+      markers.push(marker_public_art);
+    });
   };
 
   function renderDirections (pos1, pos2){
@@ -84,50 +129,6 @@ var infowindows = {};
     getNearbyPoints(pos2[0], pos2[1]);
   };
 
-  var handleArtResults = function(data, lat_starting, lng_starting){
-    console.log(data);
-    var bounds = new google.maps.LatLngBounds(new google.maps.LatLng(lat_starting - 0.01, lng_starting - 0.01), new google.maps.LatLng(lat_starting + 0.01, lng_starting + 0.01)); 
-    Object.keys(data)
-    .filter(function(title) {
-      return bounds.contains(new google.maps.LatLng(data[title].latitude, data[title].longitude));
-    })
-    .forEach(function(title){
-      var attributes = data[title];
-      console.log(attributes.url);
-      var marker_public_art = new google.maps.Marker({
-        map: map,
-        animation: google.maps.Animation.DROP,
-        position: new google.maps.LatLng(attributes.latitude, attributes.longitude),
-        icon: 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png',
-      });
-      
-      google.maps.event.addListener(marker_public_art, 'click', function() {
-        var header = $('<h3></h3>');
-        header.text(title);
-        var content = $('<div></div>');
-        var address = $('<div></div>');
-        address.text(attributes.address);
-        var exterior = $('<div></div>');
-        exterior.text(attributes.exterior);
-        var directions = $('<button class="info-window-direction-button" />');
-        directions.text('Directions');
-        content.append(header);
-        content.append(address);
-        content.append(exterior);
-        content.append(directions);
-        infowindow.setContent(content.html()); 
-        infowindow.open(map, marker_public_art);
-        $('.info-window-direction-button').click(function() {
-          renderDirections([lat_starting, lng_starting], [attributes.latitude, attributes.longitude])
-        });
-      });
-      markers.push(marker_public_art);
-
-    //google.maps.event.addListener(marker_public_art, 'mouseout', function() { 
-      //infowindow_public_art.close();  });
-    });
-  };
-
     function getNearbyPoints(lat, lon) {
       markers.forEach(function(marker) {
         marker.setMap(null);
@@ -141,8 +142,11 @@ var infowindows = {};
       markers.push(marker);
       //Now Send the user input geocoded address (lat lng) to webapp.py
       //parses the json for you instead of leaving it as a string.
-      $.getJSON("/get_nearby_businesses", {lat: lat, lng: lon}).done(handleBusinessResults);
-      $.getJSON("/get_public_art", {lat: lat, lng: lon}).done(function(data) {
+      $.getJSON("/get_nearby_businesses", {lat: lat, lng: lon}).done(function(data){
+        handleBusinessResults(data, lat, lon);
+      });
+      
+      $.getJSON("/get_public_art", {lat: lat, lng: lon}).done(function(data) { //ajax request
         handleArtResults(data, lat, lon);
       });
     }
@@ -162,11 +166,9 @@ var infowindows = {};
       else{
         console.log(status);
       }
-///////////////////////
-   // All above code and the last line: google.maps.event.addDomListener(window, 'load', initialize);  --> comes from: https://developers.google.com/maps/documentation/javascript/examples/map-simple
-  //////// Creates a new geocoder class'''
-    
-    }//end bike initialize
+    }  
+////////// All above code and the last line: google.maps.event.addDomListener(window, 'load', initialize);  --> comes from: https://developers.google.com/maps/documentation/javascript/examples/map-simple
+  //////// Below Creates a new geocoder class'''
     var geocoder = new google.maps.Geocoder();
     var geocoderRequest = {
       address: bike.address,
